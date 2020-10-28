2e4b644e1e3c9ca272f7f9382cb9b47c8fe4df59
leon-carvalho <leonardoscc15@gmail.com>
2020-10-28 10:51:12 -0300

    feat: add colors to global.styles

--- src/shared/global.styles.ts
+++ src/shared/global.styles.ts
@@ -1,10 +1,30 @@
 import { createGlobalStyle } from "styled-components";
+import media from "styled-media-query";
 
 export default createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  :root {
+    --primary: #2c2738;
+    --secondary: #756f86;
+    --muted: #7c9cbf;
+    --bright: #ffffff;
+    --shade: #dbe2ea;
+    --tint: #ebf4f8;
+    --accent: #0880ae;
+    --warning: #f2ac57;
+    --success: #14a38b;
+    --error: #ff7171; 
+
+    font-size: 62.5%;
+
+    ${media.lessThan("medium")`
+      font-size: 60%;
+    `};
   }
 
   body, input, button, textarea {
